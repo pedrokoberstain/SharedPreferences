1a15c84915f7a680d6806336fc75241627862435
Pedro Kober <pedrokoberstain2@gmail.com>
2023-06-20 17:42:15 -0300

    projeto pronto!

--- app/src/main/java/com/example/trkober_ok/Home.java
+++ app/src/main/java/com/example/trkober_ok/Home.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 public class Home extends AppCompatActivity {
 
     private Button bt_registrar;
+    private Button bt_entrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +24,22 @@ public class Home extends AppCompatActivity {
         bt_registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Intent intent = new Intent(Home.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+        bt_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, Menu.class);
                 startActivity(intent);
             }
         });
     }
 
-    private void IniciarComponentes(){
+    private void IniciarComponentes() {
         bt_registrar = findViewById(R.id.bt_registrar);
+        bt_entrar = findViewById(R.id.bt_entrar);
     }
 }
--- app/src/main/java/com/example/trkober_ok/Register.java
+++ app/src/main/java/com/example/trkober_ok/Register.java
@@ -12,58 +12,11 @@ import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
 
-    //Declarar as variaveis
-    EditText ed1,ed2,ed3,ed4,ed5;
-    Button b1;
-
-    //Declarar arquivo de preferência
-    public static final String MyPREFERENCES = "arquivo";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         getSupportActionBar().hide();
-
-        ed1=(EditText)findViewById(R.id.nome);
-        ed2=(EditText)findViewById(R.id.email);
-        ed3=(EditText)findViewById(R.id.remail);
-        ed4=(EditText)findViewById(R.id.senha);
-        ed5=(EditText)findViewById(R.id.cpf);
-
-        b1=(Button)findViewById(R.id.bt_salvar);
-
-        SharedPreferences sharedpreferences = getSharedPreferences(MyPREFERENCES, 0);
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String nome  = ed1.getText().toString();
-                String email  = ed2.getText().toString();
-                String renvemail  = ed3.getText().toString();
-                String senha  = ed3.getText().toString();
-                String cpf  = ed3.getText().toString();
-
-                SharedPreferences.Editor editor = sharedpreferences.edit();
-
-                editor.putString("Usuario", nome);
-                editor.putString("Email", email);
-                editor.putString("Confirmação do Email", renvemail);
-                editor.putString("Senha", senha);
-                editor.putString("CPF", cpf);
-                editor.commit();
-                Toast.makeText(Register.this,"Dados cadastrados no arquivo .xml com sucesso!!",Toast.LENGTH_LONG).show();
-
-                //LIMPAR O EDITTEXT
-                ed1.getText().clear();
-                ed2.getText().clear();
-                ed3.getText().clear();
-                ed4.getText().clear();
-                ed5.getText().clear();
-                ed1.requestFocus();
-
-            }
-        });
     }
 }
